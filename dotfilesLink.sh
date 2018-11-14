2072836acd08a4c136ba31772b76a7c5ca2aedf2
Salieri <salieri@NaunoMacBook-Pro.local>
2018-11-14 15:03:35 -0800

    modify sh

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,3 +1,4 @@
-$ #!/bin/sh
-$ ln -sf ~/dotfiles/_vimrc ~/.vimrc
-$ ln -sf ~/dotfiles/vimfiles ~/.vim
+#!/bin/sh
+ln -sf ~/dotfiles/_vimrc ~/.vimrc
+ln -sf ~/dotfiles/vimfiles ~/.vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/dotfiles/vimfiles/bundle/Vundle.vim
