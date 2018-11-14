0dd99cf2c98d65c82e741f06f224ecdd0d1dafc5
Salieri <salieri@NaunoMacBook-Pro.local>
2018-11-14 15:36:28 -0800

    change names

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
-ln -sf ~/dotfiles/_vimrc ~/.vimrc
-ln -sf ~/dotfiles/vimfiles ~/.vim
-git clone https://github.com/VundleVim/Vundle.vim.git ~/dotfiles/vimfiles/bundle/Vundle.vim
+unlink ~/.vimrc
+unlink ~/.vim
+ln -sf ~/dotfiles.vim/_vimrc ~/.vimrc
+ln -sf ~/dotfiles.vim/vimfiles ~/.vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/dotfiles.vim/vimfiles/bundle/Vundle.vim
