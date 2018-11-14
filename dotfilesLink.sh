a69763d23e3ffe169436261754bcb7cd9e25ffea
Salieri <salieri@NaunoMacBook-Pro.local>
2018-11-14 15:49:55 -0800

    modify ReadMe

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -4,3 +4,5 @@ unlink ~/.vim
 ln -sf ~/dotfiles.vim/_vimrc ~/.vimrc
 ln -sf ~/dotfiles.vim/vimfiles ~/.vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/dotfiles.vim/vimfiles/bundle/Vundle.vim
+vim +PluginInstall +qall
+
